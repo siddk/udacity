51b081c527df4e62aa60560b08c9bd9c07e2b5c8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 01:29:14 -0700

    Debugging

--- Web Development/AppEngine/hello-udacity/code/ascii.py
+++ Web Development/AppEngine/hello-udacity/code/ascii.py
@@ -4,6 +4,8 @@ ascii.py
 Sample web application to demonstrate basic databases in Google App Engine.
 Mockup of the website 4chan, asciiChan, a platform to share asciiArt.
 """
+from google.appengine.ext import db
+from code.handler import Handler
 
 class Art(db.Model):
     title = db.StringProperty(required = True)
