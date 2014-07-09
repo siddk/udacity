5bbf764cb1f13ff21e213cf586f660585c2b4c2a
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 01:20:17 -0700

    Adding ascii Chan

--- Web Development/AppEngine/hello-udacity/code/ascii.py
+++ Web Development/AppEngine/hello-udacity/code/ascii.py
@@ -4,3 +4,29 @@ ascii.py
 Sample web application to demonstrate basic databases in Google App Engine.
 Mockup of the website 4chan, asciiChan, a platform to share asciiArt.
 """
+
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class AsciiChanHandler(Handler):
+    def render_page(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+        self.render("ascii.html", title = title, art = art, error = error, arts = arts)
+
+    def get(self):
+        self.render_page()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/unit3/asciichan")
+        else:
+            error = "We need both a title and some artwork!"
+            self.render_page(title, art, error)
