3a262764486a24e83886087e5e4db18e024cb623
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 01:23:41 -0700

    blog.py, splitting main.py into submodules

---
+++ Web Development/AppEngine/hello-udacity/code/blog.py
@@ -0,0 +1,6 @@
+"""
+blog.py
+
+Pinnacle project of the Udacity Web Development Course CS 253. Implements a fully
+functional blog using webapp2, built on Google App Engine, with a SQLite backend.
+"""
