3455a6f3d89e88d9eb58f6ffe25b82de27c77832
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 00:18:14 -0700

    Refactor done, start with cookie

--- Web Development/AppEngine/hello-udacity/main.py
+++ Web Development/AppEngine/hello-udacity/main.py
@@ -273,7 +273,7 @@ class PermalinkHandler(BlogHandler):
 class BlogSignupHandler(Handler):
     def render_page(self, user_error="", pass_error="", verify_error="", email_error="",
                     user_val="", pass_val="", verify_val="", email_val=""):
-        self.render("signup.html", user_error = user_error, pass_error = pass_error, verify_error = verify_error, email_error = email_error, user_val = user_val, pass_val = pass_val, verify_val, email_val = email_val)
+        self.render("signup.html", user_error = user_error, pass_error = pass_error, verify_error = verify_error, email_error = email_error, user_val = user_val, pass_val = pass_val, verify_val = verify_val, email_val = email_val)
 
     def get(self):
         self.render("signup.html", )
@@ -286,38 +286,38 @@ class BlogSignupHandler(Handler):
 
         write_dict = {}
         if not (username and password and verify_password):
-            self.write_form(user_error="Missing some fields", user_val=cgi.escape(username, quote=True),
+            self.render_page(user_error="Missing some fields", user_val=cgi.escape(username, quote=True),
                 pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
                 email_val=cgi.escape(email, quote=True))
             return
 
         #Check if username is valid
         if not (re.compile(r"^[a-zA-Z0-9_-]{3,20}$")).match(username):
-            self.write_form(user_error="Invalid username", user_val=cgi.escape(username, quote=True),
+            self.render_page(user_error="Invalid username", user_val=cgi.escape(username, quote=True),
                 pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
                 email_val=cgi.escape(email, quote=True))
             return
 
         if not (re.compile(r"^.{3,20}$")).match(password):
-            self.write_form(pass_error="Invalid password", user_val=cgi.escape(username, quote=True),
+            self.render_page(pass_error="Invalid password", user_val=cgi.escape(username, quote=True),
                 pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
                 email_val=cgi.escape(email, quote=True))
             return
 
         if not (password == verify_password):
-            self.write_form(verify_error="Passwords do not match", user_val=cgi.escape(username, quote=True),
+            self.render_page(verify_error="Passwords do not match", user_val=cgi.escape(username, quote=True),
                 pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
                 email_val=cgi.escape(email, quote=True))
             return
 
         if email:
             if not (re.compile(r"^[\S]+@[\S]+\.[\S]+$")).match(email):
-                self.write_form(email_error="Invalid email", user_val=cgi.escape(username, quote=True),
+                self.render_page(email_error="Invalid email", user_val=cgi.escape(username, quote=True),
                     pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
                     email_val=cgi.escape(email, quote=True))
                 return
 
-        self.redirect("/unit2/welcome?username=%s" % cgi.escape(username, quote=True))
+
 
 SECRET = 'imsosecret'
 
@@ -355,4 +355,4 @@ class CookieHandler(Handler):
             self.write("You've been here %s times!" % visits)
 
 
-app = webapp2.WSGIApplication([('/', MainHandler), ('/unit2/rot13', ROT13Handler), ('/unit2/signup', SignupHandler), ('/unit2/welcome', WelcomeHandler), ('/unit3/hard_coded_templates', HardCodedTemplateHandler), ('/unit3/templates', TemplateHandler), ('/unit3/asciichan', AsciiChanHandler), ('/blog', BlogHandler), ('/blog/newpost', PostHandler), ('/blog/(\d+)', PermalinkHandler), ('/cookies', CookieHandler)], debug=True)
+app = webapp2.WSGIApplication([('/', MainHandler), ('/unit2/rot13', ROT13Handler), ('/unit2/signup', SignupHandler), ('/unit2/welcome', WelcomeHandler), ('/unit3/hard_coded_templates', HardCodedTemplateHandler), ('/unit3/templates', TemplateHandler), ('/unit3/asciichan', AsciiChanHandler), ('/blog', BlogHandler), ('/blog/newpost', PostHandler), ('/blog/(\d+)', PermalinkHandler), ('/blog/signup', BlogSignupHandler), ('/cookies', CookieHandler)], debug=True)
