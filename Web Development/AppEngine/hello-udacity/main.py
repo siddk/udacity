25db2879dbeb59fb16394312d2b93d03e91cd2c6
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 01:07:58 -0700

    Split main.py into submodules

--- Web Development/AppEngine/hello-udacity/main.py
+++ Web Development/AppEngine/hello-udacity/main.py
@@ -24,6 +24,7 @@ import hashlib
 import hmac
 import random
 import string
+from code.rot13 import ROT13Handler
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
@@ -43,43 +44,6 @@ class MainHandler(Handler):
     def get(self):
         self.write("Welcome to Sidd's Site for the Udacity Web Development Course CS 253")
 
-class ROT13Handler(webapp2.RequestHandler):
-    def rot13(self, cipher_text):
-        return_string = ""
-        for character in cipher_text:
-            if character.isalpha():
-                int_char = ord(character) + 13
-                if character.isupper():
-                    if int_char > ord('Z'): int_char -= 26
-                else:
-                    if int_char > ord('z'): int_char -= 26
-                return_string += chr(int_char)
-            else:
-                return_string += character
-        return return_string
-
-    def write_form(self, input_text=""):
-        input_text = self.rot13(input_text)
-        self.response.out.write(rot_13_form % {"textarea": input_text})
-
-    def get(self):
-        self.write_form()
-
-    def post(self):
-        input_text = self.request.get('text')
-        self.write_form(input_text)
-rot_13_form = """
-<form method="post">
-    Enter text to be ROT13 Encrypted:
-    <br>
-
-    <textarea name="text">%(textarea)s</textarea>
-
-    <br>
-    <br>
-    <input type="submit">
-</form>
-"""
 
 class SignupHandler(webapp2.RequestHandler):
 
@@ -322,11 +286,15 @@ class BlogSignupHandler(Handler):
 
         u = User(user = username, pw = hashlib.sha256(password).hexdigest())
         u_key = u.put()
-        u_id = u_key.id()
+        u_id = str(u_key.id())
+
 
         sts = make_secure_val(u_id)
         self.response.headers.add_header('Set-Cookie', 'visited=%s; Path=/' % sts)
         self.redirect("/blog/welcome")
+
+class BlogWelcomeHandler(Handler):
+    pass
 
 class User(db.Model):
     user = db.StringProperty(required = True)
