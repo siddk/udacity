1e7f5586a7247800603eae69882baca2b2d929fc
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-09 00:10:26 -0700

    Refactoring Unit 2 signup code

--- Web Development/AppEngine/hello-udacity/main.py
+++ Web Development/AppEngine/hello-udacity/main.py
@@ -270,6 +270,55 @@ class PermalinkHandler(BlogHandler):
         s = Post.get_by_id(int(post_id))
         self.render("blog.html", posts = [s])
 
+class BlogSignupHandler(Handler):
+    def render_page(self, user_error="", pass_error="", verify_error="", email_error="",
+                    user_val="", pass_val="", verify_val="", email_val=""):
+        self.render("signup.html", user_error = user_error, pass_error = pass_error, verify_error = verify_error, email_error = email_error, user_val = user_val, pass_val = pass_val, verify_val, email_val = email_val)
+
+    def get(self):
+        self.render("signup.html", )
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify_password = self.request.get('verify')
+        email = self.request.get('email')
+
+        write_dict = {}
+        if not (username and password and verify_password):
+            self.write_form(user_error="Missing some fields", user_val=cgi.escape(username, quote=True),
+                pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
+                email_val=cgi.escape(email, quote=True))
+            return
+
+        #Check if username is valid
+        if not (re.compile(r"^[a-zA-Z0-9_-]{3,20}$")).match(username):
+            self.write_form(user_error="Invalid username", user_val=cgi.escape(username, quote=True),
+                pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
+                email_val=cgi.escape(email, quote=True))
+            return
+
+        if not (re.compile(r"^.{3,20}$")).match(password):
+            self.write_form(pass_error="Invalid password", user_val=cgi.escape(username, quote=True),
+                pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
+                email_val=cgi.escape(email, quote=True))
+            return
+
+        if not (password == verify_password):
+            self.write_form(verify_error="Passwords do not match", user_val=cgi.escape(username, quote=True),
+                pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
+                email_val=cgi.escape(email, quote=True))
+            return
+
+        if email:
+            if not (re.compile(r"^[\S]+@[\S]+\.[\S]+$")).match(email):
+                self.write_form(email_error="Invalid email", user_val=cgi.escape(username, quote=True),
+                    pass_val=cgi.escape(password, quote=True), verify_val=cgi.escape(verify_password, quote=True),
+                    email_val=cgi.escape(email, quote=True))
+                return
+
+        self.redirect("/unit2/welcome?username=%s" % cgi.escape(username, quote=True))
+
 SECRET = 'imsosecret'
 
 def hash_str(s):
