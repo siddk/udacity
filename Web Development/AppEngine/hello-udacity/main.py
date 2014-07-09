2454b493c94f1a394559be570feb81a2fdc2e176
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 23:04:31 -0700

    Hash implementation

--- Web Development/AppEngine/hello-udacity/main.py
+++ Web Development/AppEngine/hello-udacity/main.py
@@ -273,10 +273,10 @@ def hash_str(s):
     return hashlib.md5(s).hexdigest()
 
 def make_secure_val(s):
-    return s + "," + hash_str(s)
+    return s + "|" + hash_str(s)
 
 def check_secure_val(h):
-    splitr = h.split(',')
+    splitr = h.split('|')
     if (hash_str(splitr[0]) == splitr[1]):
         return splitr[0]
     else:
@@ -285,14 +285,17 @@ def check_secure_val(h):
 class CookieHandler(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        visits = self.request.cookies.get('visits', '0')
+        visits = 0
+        visit_cookie_str = self.request.cookies.get('visits', '0')
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
 
-        if visits.isdigit():
-            visits = int(visits) + 1
-        else:
-            visits = 0
+        visits += 1
 
-        self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+        new_cookie_val = make_secure_val(str(visits))
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
 
         # You can cheat this, by using the JS console, and just setting the cookie
         if visits > 100000:
