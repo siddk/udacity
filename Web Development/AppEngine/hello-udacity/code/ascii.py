d7106cbf74c479111aa8441a638e1f2bd08b082f
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-10 18:48:58 -0700

    Adding geotag info

--- Web Development/AppEngine/hello-udacity/code/ascii.py
+++ Web Development/AppEngine/hello-udacity/code/ascii.py
@@ -6,15 +6,36 @@ Mockup of the website 4chan, asciiChan, a platform to share asciiArt.
 """
 from google.appengine.ext import db
 from code.handler import Handler
+import urllib2
+from xml.dom import minidom
 
 class Art(db.Model):
     title = db.StringProperty(required = True)
     art = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+    url = IP_URL + ip
+    content = None
+    try:
+        content = urllib2.urlopen(url).read()
+    except URLError:
+        return
+
+    if content:
+        #Parse xml and find the coordinates
+        x = minidom.parseString(content)
+        x = x.getElementsByTagName("gml:coordinates")
+        if x:
+            cord = x[0].childNodes[0].nodeValue
+            c = cord.split(',')
+            return db.GeoPt(c[1], c[0])
+
+
 class AsciiChanHandler(Handler):
     def render_page(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 10")
         self.render("ascii.html", title = title, art = art, error = error, arts = arts)
 
     def get(self):
@@ -26,6 +47,8 @@ class AsciiChanHandler(Handler):
 
         if title and art:
             a = Art(title = title, art = art)
+            # Lookup user's coordinates from their IP
+            # If we have coordinates, add them to Art
             a.put()
 
             self.redirect("/unit3/asciichan")
