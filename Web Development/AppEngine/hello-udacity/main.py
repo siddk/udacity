aeaf6a5ec51d6f3188076fb5800e542dc410ec7b
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-12 00:04:23 -0700

    Working Unit 6

--- Web Development/AppEngine/hello-udacity/main.py
+++ Web Development/AppEngine/hello-udacity/main.py
@@ -6,7 +6,7 @@ Renders a webapp2 web application, for the Udacity Web Development Course CS 253
 Built on Google App Engine, with jinja2 templating, and a SQLite backend.
 """
 from code.ascii import AsciiChanHandler
-from code.blog import BlogHandler, PostHandler, PermalinkHandler, BlogSignupHandler, BlogWelcomeHandler, BlogLoginHandler, BlogLogoutHandler, JSONHandler, PermalinkJSONHandler
+from code.blog import BlogHandler, PostHandler, PermalinkHandler, BlogSignupHandler, BlogWelcomeHandler, BlogLoginHandler, BlogLogoutHandler, JSONHandler, PermalinkJSONHandler, FlushHandler
 from code.cookie import CookieHandler
 from code.handler import Handler
 from code.rot13 import ROT13Handler
@@ -18,4 +18,4 @@ class MainHandler(Handler):
     def get(self):
         self.write("Welcome to Sidd's Site for the Udacity Web Development Course CS 253")
 
-app = webapp2.WSGIApplication([('/', MainHandler), ('/unit2/rot13', ROT13Handler), ('/unit2/signup', SignupHandler), ('/unit2/welcome', WelcomeHandler), ('/unit3/hard_coded_templates', HardCodedTemplateHandler), ('/unit3/templates', TemplateHandler), ('/unit3/asciichan', AsciiChanHandler), ('/blog', BlogHandler), ('/blog/.json', JSONHandler), ('/blog/newpost', PostHandler), ('/blog/(\d+)', PermalinkHandler), ('/blog/(\d+).json', PermalinkJSONHandler), ('/blog/signup', BlogSignupHandler), ('/blog/welcome', BlogWelcomeHandler), ('/blog/login', BlogLoginHandler), ('/blog/logout', BlogLogoutHandler), ('/cookies', CookieHandler)], debug=True)
+app = webapp2.WSGIApplication([('/', MainHandler), ('/unit2/rot13', ROT13Handler), ('/unit2/signup', SignupHandler), ('/unit2/welcome', WelcomeHandler), ('/unit3/hard_coded_templates', HardCodedTemplateHandler), ('/unit3/templates', TemplateHandler), ('/unit3/asciichan', AsciiChanHandler), ('/blog', BlogHandler), ('/blog/.json', JSONHandler), ('/blog/newpost', PostHandler), ('/blog/(\d+)', PermalinkHandler), ('/blog/(\d+).json', PermalinkJSONHandler), ('/blog/signup', BlogSignupHandler), ('/blog/welcome', BlogWelcomeHandler), ('/blog/login', BlogLoginHandler), ('/blog/logout', BlogLogoutHandler), ('/blog/flush', FlushHandler), ('/cookies', CookieHandler)], debug=True)
