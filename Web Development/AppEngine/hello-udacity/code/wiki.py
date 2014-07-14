9da07e5368f50f44246f170fcf156541f28f5d65
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-13 17:27:10 -0700

    Updating project description

---
+++ Web Development/AppEngine/hello-udacity/code/wiki.py
@@ -0,0 +1,6 @@
+"""
+wiki.py
+
+Final project for Udacity course CS 253. Implements a fully editable wiki, with page creation, and page editing fully available. Built with webapp2, deployed on Google App Engine, with SQLite backend.
+"""
+
